8f81ed44717dd2123fadcd31332722c72644287d
Jonathan Gerber <jlgerber@gmail.com>
2021-02-19 08:14:07 -0800

    updates docs on FrmStr trait

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,11 @@ use error::PesError;
 use pubgrub::range::Range;
 use pubgrub::version::SemanticVersion;
 
+/// Trait to provide an alternative, falible constructor from a &str
 pub trait FrmStr {
     type FrmStrErr;
 
+    /// Given a str, construct an instance of Self
     fn frm_str(value: &str) -> Result<Self, Self::FrmStrErr> where Self: Sized;
 }
 
