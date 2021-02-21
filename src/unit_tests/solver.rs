90e4db65ea05c60641b1e82c1480b68669176b0f
Jonathan Gerber <jlgerber@gmail.com>
2021-02-20 17:30:33 -0800

    updates solver unit tests

--- src/unit_tests/solver.rs
+++ src/unit_tests/solver.rs
@@ -2,12 +2,12 @@
 
 use super::*;
 use std::path::PathBuf;
-use pubgrub::report::DefaultStringReporter;
-use pubgrub::error::PubGrubError;
-
 use crate::versioned_package::VersionedPackage;
 use crate::repository::PackageRepository;
-use crate::error::PesError;
+
+//-------------//
+//   HELPERS   //
+//-------------//
 
 fn get_repo_root(repo_name: &str) -> PathBuf {
     let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -18,11 +18,16 @@ fn get_repo_root(repo_name: &str) -> PathBuf {
 
 const MANI: &'static str = "manifest.yaml";
 
+
+//-------------//
+//    TESTS    //
+//-------------//
+
 #[test]
-fn add_repository__given_a_repository_with_a_package_without_run_target() {
+fn add_repository__given_a_repository_with_a_package_without_run_target__succeeds() {
     let pkg_repo = PackageRepository::new(get_repo_root("repo2"), MANI);
     let mut solver = Solver::new();
-    solver.add_repository(&pkg_repo);
+    solver.add_repository(&pkg_repo).expect("should be able to add repository");
     let versions: Vec<&SemanticVersion> = solver.versions("foo").unwrap().collect();
     assert_eq!(versions.len(),1);
     let solution =  solver.solve_from_str("foo-0.1.0");
@@ -31,7 +36,7 @@ fn add_repository__given_a_repository_with_a_package_without_run_target() {
 }
 
 #[test]
-fn convert_request_str__given_space_separated_list() {
+fn convert_request_str__given_space_separated_list__succeeds() {
     let request = "maya  maya_plugins-1.2.3+<3";
     let result = Solver::convert_request_str(request);
     assert_eq!(result, vec![
@@ -41,25 +46,12 @@ fn convert_request_str__given_space_separated_list() {
 }
 
 
-// #[test]
-// fn solver_test_01() {
-    
-//     let package_repo = PackageRepository::new(get_repo_root("repo"), MANI);
-//     // let manifests: Vec<PathBuf> = package_repo.manifests().filter_map(|x| x.ok()).collect();
-//     // assert_eq!(manifests, Vec::<PathBuf>::new());
-//     let mut solver = Solver::new();
-//     solver.add_repository(&package_repo);
-//     let versions: Vec<&SemanticVersion> = solver.versions("foo").unwrap().collect();
-//     assert_eq!(versions, Vec::<&SemanticVersion>::new());
-// }
 #[test]
-fn solver_test_02() {
+fn solve_from_str__given_a_valid_distribution__succeeds() {
     
     let package_repo = PackageRepository::new(get_repo_root("repo"), MANI);
     let mut solver = Solver::new();
-    solver.add_repository(&package_repo);
+    solver.add_repository(&package_repo).expect("should be able to add repository");
     let solution =  solver.solve_from_str("bar-0.1.0");
     assert!(solution.is_ok());
-
-   // assert_eq!(solution.unwrap_err().to_string(), "foo".to_string());
 }
