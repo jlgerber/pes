822e76aa30e73a60898d8aebdeed45b3fe5e8268
Jonathan Gerber <jlgerber@gmail.com>
2021-02-20 21:52:53 -0800

    remove FrmStr trait from range.rs

--- src/range.rs
+++ src/range.rs
@@ -1,16 +1,9 @@
-use crate::parser::parse_consuming_semver_range;
 use crate::error::PesError;
+use crate::parser::parse_consuming_semver_range;
+use crate::traits::FrmStr;
 
 use pubgrub::range::Range;
 use pubgrub::version::SemanticVersion;
-
-/// Trait to provide an alternative, falible constructor from a &str
-pub trait FrmStr {
-    type FrmStrErr;
-
-    /// Given a str, construct an instance of Self
-    fn frm_str(value: &str) -> Result<Self, Self::FrmStrErr> where Self: Sized;
-}
 
 pub type SemVerRange = Range<SemanticVersion>;
 
