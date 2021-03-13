b885353503939ffca6f013ed0726441d72c82b9b
Jonathan Gerber <jlgerber@gmail.com>
2021-03-12 16:38:19 -0800

    cleans up and simplifies plugin_mgr

--- peslib/src/plugin_mgr.rs
+++ peslib/src/plugin_mgr.rs
@@ -18,8 +18,6 @@ impl PluginMgr {
     #[cfg(not(feature="segfault"))]
     pub fn new() -> Result<Self, PesError> {
         info!("building pluginmgr");
-        //let repo_finder = Self::new_repo_finder_service()?;
-        //Ok(Self { repo_finder })
         Ok(Self {})
     }
 
@@ -32,25 +30,22 @@ impl PluginMgr {
 
     /// retrieve a manifest given a distribution
     pub fn manifest_path_from_distribution<D: Into<PathBuf>>(&self, distribution: D) -> PathBuf {
-        #[cfg(target_os = "macos")]
+        
         let dso_path = std::env::var(MANIFEST_FINDER_VARNAME)
             .unwrap_or_else(|_| {
                 let mut path = std::env::current_exe().expect("cannot get current executable from env");
                 path.pop();
                 path.push("../lib");
+                
+                #[cfg(target_os = "macos")]
                 path.push("libmanifest_finder.dylib");
+
+                #[cfg(target_os = "linux")]
+                path.push("libmanifest_finder.so");
+                
                 path.into_os_string().into_string().expect("cannot convert path to string")
             });
 
-        #[cfg(target_os = "linux")]
-        let dso_path = std::env::var(MANIFEST_FINDER_VARNAME)
-            .unwrap_or_else(|_| {
-                let mut path = std::env::current_exe().expect("cannot get current executable from env");
-                path.pop();
-                path.push("../lib");
-                path.push("libmanifest_finder.so");
-                path.into_os_string().into_string().expect("cannot convert path to string")
-            });
         info!("loading {:?}", &dso_path);
         let lib = unsafe { libloading::Library::new(dso_path.as_str()).expect("unable to load lib") };
         
@@ -66,8 +61,6 @@ impl PluginMgr {
     #[cfg(feature="segfault")]
     pub fn repos(&self) -> Vec<std::path::PathBuf> {
         info!("calling find_repo");
-        //let repo = self.repo_finder.find_repo();
-        //let repo_finder = self.new_repo_finder_service2().expect("unable to get plugin");
         let repo = self.repo_finder.find_repo();
         repo
     }
@@ -75,25 +68,22 @@ impl PluginMgr {
     /// retrieve a list of paths to package repositories
     #[cfg(not(feature="segfault"))]
     pub fn repos(&self) -> Vec<std::path::PathBuf> {
-        #[cfg(target_os = "macos")]
+        
         let dso_path = std::env::var(REPO_FINDER_VARNAME)
             .unwrap_or_else(|_| {
                 let mut path = std::env::current_exe().expect("cannot get current executable from env");
                 path.pop();
                 path.push("../lib");
+
+                #[cfg(target_os = "macos")]
                 path.push("librepo_finder.dylib");
+
+                #[cfg(target_os = "linux")]
+                path.push("librepo_finder.so");
+                
                 path.into_os_string().into_string().expect("cannot convert path to string")
             });
 
-        #[cfg(target_os = "linux")]
-        let dso_path = std::env::var(REPO_FINDER_VARNAME)
-            .unwrap_or_else(|_| {
-                let mut path = std::env::current_exe().expect("cannot get current executable from env");
-                path.pop();
-                path.push("../lib");
-                path.push("librepo_finder.so");
-                path.into_os_string().into_string().expect("cannot convert path to string")
-            });
         info!("loading {:?}", &dso_path);
         let lib = unsafe { libloading::Library::new(dso_path.as_str()).expect("unable to load lib") };
         
