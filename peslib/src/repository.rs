89bc6af37f72f69a08a70e282db900d8a73e3581
Jonathan Gerber <jlgerber@gmail.com>
2021-03-13 16:10:24 -0800

    removes manifest field from PackageRepository

--- peslib/src/repository.rs
+++ peslib/src/repository.rs
@@ -23,14 +23,12 @@ pub struct PackageRepository<'a> {
     // we expect the repository to be laid out like so:
     // /root/<package>/<version>/manifest.yaml
     root: PathBuf,
-    /// the manifest name, including any subdirectories under the version
-    manifest: String,
+    /// plugin manager from which we get the list of potential repository locations
+    /// and the relative path to the package manifest from the distribution root
     plugin_mgr: &'a PluginMgr,
 }
 
-// todo: Repository should be responsible for finding the path to a distribution (specific package
-// version), not the manifest itself. a ManifestLocator should be responsible for taking a pathbuf
-// to a distribution and producing a pathbuf pointing at a manifest
+
 impl<'a> Repository for PackageRepository<'a> {
     type Manifest = PathBuf;
     type Err = PesError;
@@ -104,10 +102,9 @@ impl<'a> Repository for PackageRepository<'a> {
 
 impl<'a> PackageRepository<'a> {
     /// construct a new PackageRepository instance
-    pub fn new<P: Into<PathBuf>, M: Into<String>>(root: P, manifest: M, plugin_mgr: &'a PluginMgr) -> Self {
+    pub fn new<P: Into<PathBuf>>(root: P,  plugin_mgr: &'a PluginMgr) -> Self {
         Self {
             root: root.into(),
-            manifest: manifest.into(),
             plugin_mgr
         }
     }
@@ -116,25 +113,6 @@ impl<'a> PackageRepository<'a> {
         return &self.root.as_path();
     }
 
-    // /// Retrieve the location(s) of package repositories from the environment and
-    // /// return a vector of them, assuming they exist. If no repos are found, then
-    // /// return an Error.
-    // pub fn from_env() -> Result<Vec<PackageRepository>, PesError> {
-    //     let repos_env = std::env::var(PACKAGE_REPO_PATH_VAR_NAME)?;
-    //     // construct a vector of repos
-    //     let repos = repos_env
-    //         .split(":")
-    //         .map(|x| Path::new(x))
-    //         .filter_map(|x| (if x.exists() { Some(x) } else { None }))
-    //         .map(|x| Self::new(x, MANIFEST_NAME))
-    //         .collect::<Vec<_>>();
-    //     if repos.len() == 0 {
-    //         Err(PesError::NoRepositories(repos_env))
-    //     } else {
-    //         Ok(repos)
-    //     }
-    // }
-
     /// Retrieve the locatons of package repositories from the plugin
     pub fn from_plugin(plugin_mgr: &'a PluginMgr) -> Result<Vec<PackageRepository>, PesError> {
         //let repos = Self::find_repos_via_plugin()?;
@@ -142,7 +120,7 @@ impl<'a> PackageRepository<'a> {
         let repos = repos
             .iter()
             .filter_map(|x| (if x.exists() { Some(x) } else { None }))
-            .map(|x| Self::new(x, MANIFEST_NAME, &plugin_mgr))
+            .map(|x| Self::new(x, &plugin_mgr))
             .collect::<Vec<_>>();
         Ok(repos)
     }
